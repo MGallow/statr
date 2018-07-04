f9c59a1471ed8223ebd7ff9d94da8e7fc846d040
MGallow <gall0441@umn.edu>
2018-07-04 15:19:39 -0500

    bug fix

--- R/data_gen.R
+++ R/data_gen.R
@@ -17,8 +17,9 @@
 #' @export
 
 # we define the data generation function
-data_gen = function(n, p, r = 1, sparsity = 0.5, Sigma = c("tridiag", "dense", "denseQR", "compound"), 
-    s = NULL, SigmaX = c("tridiag", "dense", "denseQR", "compound"), sx = NULL, ...) {
+data_gen = function(n, p, r = 1, sparsity = 0.5, Sigma = c("tridiag", 
+    "dense", "denseQR", "compound"), s = NULL, SigmaX = c("tridiag", 
+    "dense", "denseQR", "compound"), sx = NULL, ...) {
     
     # checks
     SigmaX = match.arg(SigmaX)
@@ -26,11 +27,13 @@ data_gen = function(n, p, r = 1, sparsity = 0.5, Sigma = c("tridiag", "dense", "
     
     # randomly generate betas
     betas = matrix(rnorm(p * r, 0, sqrt(1/p)), nrow = p, ncol = r)
-    betas = betas * matrix(rbinom(p * r, 1, prob = sparsity), nrow = p, ncol = r)
+    betas = betas * matrix(rbinom(p * r, 1, prob = sparsity), 
+        nrow = p, ncol = r)
     
     # generate data X
-    SigmaX = switch(SigmaX, tridiag = tridiag(p = p, n = n, ...), dense = dense(p = p, n = n, ...), 
-        denseQR = denseQR(p = p, n = n, ...), compound = compound(p = p, n = n))
+    SigmaX = switch(SigmaX, tridiag = tridiag(p = p, n = n, ...), 
+        dense = dense(p = p, n = n, ...), denseQR = denseQR(p = p, 
+            n = n, ...), compound = compound(p = p, n = n))
     X = SigmaX$X
     
     SigmaX = SigmaX$S
@@ -39,8 +42,9 @@ data_gen = function(n, p, r = 1, sparsity = 0.5, Sigma = c("tridiag", "dense", "
     }
     
     # generate sigma matrix
-    Sigma = switch(Sigma, tridiag = tridiag(p = r, n = n, ...), dense = dense(p = r, n = n, ...), 
-        denseQR = denseQR(p = r, n = n, ...), compound = compound(p = r, n = n))
+    Sigma = switch(Sigma, tridiag = tridiag(p = r, n = n, ...), 
+        dense = dense(p = r, n = n, ...), denseQR = denseQR(p = r, 
+            n = n, ...), compound = compound(p = r, n = n))
     
     Sigma = Sigma$S
     if (!is.null(s)) {
@@ -54,7 +58,8 @@ data_gen = function(n, p, r = 1, sparsity = 0.5, Sigma = c("tridiag", "dense", "
     Y = X %*% betas + Z %*% Sigma.sqrt
     
     
-    returns = list(Y = Y, X = X, betas = betas, Sigma = Sigma, SigmaX = SigmaX)
+    returns = list(Y = Y, X = X, betas = betas, Sigma = Sigma, 
+        SigmaX = SigmaX)
     return(returns)
     
 }
--- R/regression.R
+++ R/regression.R
@@ -11,27 +11,29 @@
 #' @export
 
 # we define the ridge regression function
-RIDGE = function(X, Y, lam = NULL, intercept = FALSE, standardize = FALSE){
-  
-  # which family?
-  if (ncol(Y) > 1){
-    family = "mgaussian"
-  } else {
-    family = "gaussian"
-  }
-  
-  # is lam specified?
-  if (is.null(lam)){
-    lam =  glmnet::cv.glmnet(x = X, y = Y, alpha = 0, intercept = intercept, family = family, standardize = standardize)$lambda.min
-  }
-  
-  # calculate coefficients
-  betas = glmnet(x = X, y = Y, standardize = standardize, intercept = intercept, family = family, alpha = 0, lambda = lam)
-  betas = as.matrix(do.call(cbind, coef(betas))[-1,])
-  
-  returns = list(betas = betas, lam = lam)
-  return(returns)
-  
+RIDGE = function(X, Y, lam = NULL, intercept = FALSE, standardize = FALSE) {
+    
+    # which family?
+    if (ncol(Y) > 1) {
+        family = "mgaussian"
+    } else {
+        family = "gaussian"
+    }
+    
+    # is lam specified?
+    if (is.null(lam)) {
+        lam = glmnet::cv.glmnet(x = X, y = Y, alpha = 0, intercept = intercept, 
+            family = family, standardize = standardize)$lambda.min
+    }
+    
+    # calculate coefficients
+    betas = glmnet::glmnet(x = X, y = Y, standardize = standardize, 
+        intercept = intercept, family = family, alpha = 0, lambda = lam)
+    betas = as.matrix(do.call(cbind, coef(betas))[-1, ])
+    
+    returns = list(betas = betas, lam = lam)
+    return(returns)
+    
 }
 
 
@@ -50,27 +52,29 @@ RIDGE = function(X, Y, lam = NULL, intercept = FALSE, standardize = FALSE){
 #' @export
 
 # we define the ridge regression function
-LASSO = function(X, Y, lam = NULL, intercept = FALSE, standardize = FALSE){
-  
-  # which family?
-  if (ncol(Y) > 1){
-    family = "mgaussian"
-  } else {
-    family = "gaussian"
-  }
-  
-  # is lam specified?
-  if (is.null(lam)){
-    lam =  glmnet::cv.glmnet(x = X, y = Y, alpha = 1, intercept = intercept, family = family, standardize = standardize)$lambda.min
-  }
-  
-  # calculate coefficients
-  betas = glmnet(x = X, y = Y, standardize = standardize, intercept = intercept, family = family, alpha = 1, lambda = lam)
-  betas = as.matrix(do.call(cbind, coef(betas))[-1,])
-  
-  returns = list(betas = betas, lam = lam)
-  return(returns)
-  
+LASSO = function(X, Y, lam = NULL, intercept = FALSE, standardize = FALSE) {
+    
+    # which family?
+    if (ncol(Y) > 1) {
+        family = "mgaussian"
+    } else {
+        family = "gaussian"
+    }
+    
+    # is lam specified?
+    if (is.null(lam)) {
+        lam = glmnet::cv.glmnet(x = X, y = Y, alpha = 1, intercept = intercept, 
+            family = family, standardize = standardize)$lambda.min
+    }
+    
+    # calculate coefficients
+    betas = glmnet::glmnet(x = X, y = Y, standardize = standardize, 
+        intercept = intercept, family = family, alpha = 1, lambda = lam)
+    betas = as.matrix(do.call(cbind, coef(betas))[-1, ])
+    
+    returns = list(betas = betas, lam = lam)
+    return(returns)
+    
 }
 
 
@@ -86,10 +90,10 @@ LASSO = function(X, Y, lam = NULL, intercept = FALSE, standardize = FALSE){
 #' @export
 
 # we define the frobenius norm function
-fro = function(X){
-  
-  mean(X^2)
-  
+fro = function(X) {
+    
+    mean(X^2)
+    
 }
 
 
@@ -107,27 +111,28 @@ fro = function(X){
 #' @export
 
 # we define the CVsplit function
-CVsplit = function(X, Y, split = 0.5){
-  
-  # checks
-  if ((split <= 0) || (split >= 1)){
-    stop("split must be contained in c(0, 1)!")
-  }
-
-  # specify leave.out
-  leave.out = samply(nrow(X), floor(nrow(X)*split))
-  
-  # training sets
-  X.train = X[leave.out,]
-  Y.train = Y[leave.out,]
-  
-  # testing sets
-  X.test = X[-leave.out,]
-  Y.test = Y[-leave.out,]
-  
-  returns = list(X.train = X.train, Y.train = Y.train, X.test = X.test, Y.test = Y.test)
-  return(returns)
-  
+CVsplit = function(X, Y, split = 0.5) {
+    
+    # checks
+    if ((split <= 0) || (split >= 1)) {
+        stop("split must be contained in c(0, 1)!")
+    }
+    
+    # specify leave.out
+    leave.out = samply(nrow(X), floor(nrow(X) * split))
+    
+    # training sets
+    X.train = X[leave.out, ]
+    Y.train = Y[leave.out, ]
+    
+    # testing sets
+    X.test = X[-leave.out, ]
+    Y.test = Y[-leave.out, ]
+    
+    returns = list(X.train = X.train, Y.train = Y.train, X.test = X.test, 
+        Y.test = Y.test)
+    return(returns)
+    
 }
 
 
